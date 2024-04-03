48696f9eb9e38bf8912ca720cbae527479611bd6
Jesus Rojas <jrojas1@mfvfhv0ayq05d.ad.corp.local>
2024-04-03 18:25:36 -0300

    ejercicios 1 y 2

---
+++ ejercicio1-red-social/src/test/java/ar/edu/unlp/info/oo2/MainTest.java
@@ -0,0 +1,11 @@
+package ar.edu.unlp.info.oo2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
